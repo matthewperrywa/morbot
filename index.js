bed4816e38a2674b959c86c869737585440e53ce
matthewperrywa <matthew.c.perry@outlook.com>
2022-07-27 20:47:39 -0700

    initialize github repository

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}`);
+})
+
+client.on("messageCreate", (message) => {
+    if(message.content == "hi"){
+        message.reply("I am Dr. Michael Morbius.")
+    }
+    else if(message.content == "morb"){
+        message.reply("https://cdn.discordapp.com/attachments/935989994735169546/988187107300352070/full-1.webm")
+    }
+    else if(message.content == "banana"){
+        message.reply("It's Minions time.")
+    }
+    else if(message.content != "It's Morbin time." &&
+            message.content != "I am Dr. Michael Morbius." &&
+            message.content != "https://cdn.discordapp.com/attachments/935989994735169546/988187107300352070/full-1.webm" &&
+            message.content != "It's Minions time."){
+        message.reply("It's Morbin time.")
+    }
+})
+
+client.login(process.env.TOKEN)
